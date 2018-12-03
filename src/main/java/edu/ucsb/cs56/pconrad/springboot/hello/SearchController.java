60c0d3715a050dea89669edab088dad0f605004f
Joshandrews43 <joshandrews43@hotmail.com>
2018-12-02 16:27:22 -0800

    JA: Created search result object and updated code for sending data to frontend

--- src/main/java/edu/ucsb/cs56/pconrad/springboot/hello/SearchController.java
+++ src/main/java/edu/ucsb/cs56/pconrad/springboot/hello/SearchController.java
@@ -1,6 +1,7 @@
 package edu.ucsb.cs56.pconrad.springboot.hello;
 
 import edu.ucsb.cs56.pconrad.springboot.hello.SearchQuery;
+import edu.ucsb.cs56.pconrad.springboot.hello.SearchResult;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,7 +9,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 
 import java.util.stream.Collectors;
+import java.util.ArrayList;
 import javax.validation.Valid;
+
 
 @RestController
 public class SearchController{
@@ -19,21 +22,25 @@ public class SearchController{
 		SearchQuery query = new SearchQuery(rawQuery);
 		String errorMsg = new String();
 		// We will eventually return something more complex than a String
-		String results = new String();
-		
+		ArrayList<String> results = new ArrayList<String>();
+
 		if(errors.hasErrors()) {
 			errorMsg = (errors.getAllErrors().stream().map(x->x.getDefaultMessage()).collect(Collectors.joining(",")));
 			return ResponseEntity.badRequest().body(errorMsg);
 		}
 
 		if(query.getEngine().equals("Google")){
-			results = "Searched " + query.getUserEntry() + " with Google";
+			//  this is just an example. Individual search api functions should return an ArrayList of Strings, formatted as
+			//  "title:subtitle:URL" which can be easily achieved with the toSplittableString() method in the SearchResult Object.
+			// then set results = YourEngineFunctionThatReturnsAnArrayListOfStrings() and it should work.
+			SearchResult resultsObject = new SearchResult("title", "subtitle","URL");
+			results.add(resultsObject.toSplittableString());
 		}
-		else if(query.getEngine() == "DuckDuckGo"){
-			results = "Searched " + query.getUserEntry() + " with DuckDuckGo";
+		else if(query.getEngine().equals("DuckDuckGo")){
+			//results = "Searched " + query.getUserEntry() + " with DuckDuckGo";
 		}
-		else if(query.getEngine() == "Bing"){
-			results = "Searched " + query.getUserEntry() + " with Bing";
+		else if(query.getEngine().equals("Bing")){
+			//results = "Searched " + query.getUserEntry() + " with Bing";
 		}
 
 		return ResponseEntity.ok(results);
