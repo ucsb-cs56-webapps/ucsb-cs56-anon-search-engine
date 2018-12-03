db320b83497bca8a044a7fe9b45444e638f1e4ab
Matt Moss <matthewmoss@me.com>
2018-12-03 14:52:41 -0800

    fixed query

--- src/main/java/edu/ucsb/cs56/pconrad/springboot/hello/DuckDuckGoSearchManager.java
+++ src/main/java/edu/ucsb/cs56/pconrad/springboot/hello/DuckDuckGoSearchManager.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import javax.net.ssl.HttpsURLConnection;
+import javax.tools.JavaFileObject;
+
+import com.fasterxml.jackson.core.JsonParser;
 
 public class DuckDuckGoSearchManager {
 
@@ -11,13 +14,14 @@ public class DuckDuckGoSearchManager {
         System.out.println("starting query!");
 
         //https://api.duckduckgo.com/?no_redirect=1&no_html=1&skip_disambig=1&q=DuckDuckGo
-        URL url = new URL("https://api.duckduckgo.com/?no_redirect=1&no_html=1&skip_disambig=1&q=DuckDuckGo");
+        URL url = new URL("https://api.duckduckgo.com/?q=DuckDuckGo&format=json");
 
         // Set up connects
 
         HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+        connection.setRequestProperty("User-Agent", "UCSB/1.0");
         // connection.setRequestMethod("GET");
-        connection.setRequestProperty("Content-Type", "application/json");
+        // connection.setRequestProperty("Content-Type", "application/json");
         System.out.println(connection.getResponseMessage());
 
         // Read output
@@ -31,7 +35,14 @@ public class DuckDuckGoSearchManager {
         input.close();
         connection.disconnect();
 
+        // Parse results
+
+        JsonParser parser = new JsonParser();
+		JavaFileObject json = parser.parse(content.toString()).getAsJsonObject();
+
         // Print results
+
+
 
         System.out.println("did get response: " + content.toString());
     }
